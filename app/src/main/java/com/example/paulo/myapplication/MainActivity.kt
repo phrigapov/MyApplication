8a00dae924ec229874893f00a089b486ec0a5c31
Paulo Henrique <phrigapov@gmail.com>
2017-07-08 17:03:22 -0300

    Teste agora

--- app/src/main/java/com/example/paulo/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/paulo/myapplication/MainActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-
+//teste
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
